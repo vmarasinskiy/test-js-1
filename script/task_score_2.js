65ae4fc8d002c4977ac753f531fc396d5660f800
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-18 14:48:09 +0300

    add_file_task_score_2_2.1.5

---
+++ script/task_score_2.js
@@ -0,0 +1,10 @@
+let value1 = 100;
+let value2 = Math.floor(Math.random() * 150);
+
+let total = value1 + value2
+
+console.log(total/ 5 + 17)
+
+/*Приоритет операторов.
+Операторы с более высоким приоритетом выполняются первыми.
+*/
