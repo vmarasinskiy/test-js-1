6b89ec831818f50e2d1457e628218f943dcfb5ba
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-18 07:42:47 +0300

    add file task_score

---
+++ script/task_score.js
@@ -0,0 +1,17 @@
+//очки за каждое задание
+let introductionScore = 50;
+let gitScore = 100;
+let jsScore = 150;
+
+//расчет среднего значения
+let averageScore = (introductionScore + gitScore + jsScore) / 3;
+
+//Студент
+let firstName = "Victor";
+let lastName = "Marasinskiy";
+
+console.log("Студент: " + firstName + " " + lastName);
+console.log("Баллы по модулю Introduction: " + introductionScore);
+console.log("Баллы по модулю Git: " + gitScore);
+console.log("Баллы по модулю JavaScript: " + jsScore);
+console.log("Средний балл: " + averageScore);
